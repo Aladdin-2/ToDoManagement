3bc90e25c6f53d21ac3b1ebc754c09437388b881
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-01-27 18:18:23 +0400

    Task completed successfully!

--- src/ApplicationRun.java
+++ src/ApplicationRun.java
@@ -8,17 +8,28 @@ public class ApplicationRun {
     public static void main(String[] args) {
         CommonRepository commonRepository = new CommonRepository();
         commonRepository.getTodos().add(new Todo("Ders", "aladdin@gamil.com", "deersi oxud"));
-
-        commonRepository.getUsers().add(new User("Asif Aliyev", "1234", "aladdinasd@gmail.com"));
+        commonRepository.getUsers().add(new User("Asif Aliyev", "1234", "aladdin@gmail.com"));
 
 
         UserService userService = new UserService(commonRepository);
-        ToDoService toDoService = new ToDoService(commonRepository);
         userService.addUser(new User("Aladdin Biyabangerd", "1234", "aladdin@gmail.com"));
 
-        toDoService.addTodo(new Todo("Ders", "aladdin@gamil.com", "deersi oxud"));
 
+        userService.printAllUser();
+        System.out.println("=============");
+        userService.removeUser("aladdin@gmail.com");
+        System.out.println("=============");
 
+        System.out.println("=============");
+        System.out.println("=============");
+
+        ToDoService toDoService = new ToDoService(commonRepository);
+        toDoService.addTodo(new Todo("Is","azer@gmail.com","Ise get"));
+
+        System.out.println("==============");
+        System.out.println("==============");
+        System.out.println("==============");
+        System.out.println("==============");
 
     }
 }
--- src/service/ToDoService.java
+++ src/service/ToDoService.java
@@ -2,11 +2,14 @@ package service;
 
 import excpetions.NotFound;
 import excpetions.ToDoNotFound;
+import excpetions.UserNotFound;
 import model.Todo;
+import model.User;
 import repository.CommonRepository;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ToDoService {
@@ -46,12 +49,9 @@ public class ToDoService {
     public String removeTodo(UUID id) {
         List<Todo> todos = commonRepository.getTodos();
         if (!todos.isEmpty()) {
-            todos
-                    .stream()
-                    .filter(todo -> Objects.equals(todo.getId(), id))
-                    .findFirst().ifPresentOrElse(todos::remove, () -> {
-                        throw new ToDoNotFound("User with id " + id + " not found.");
-                    });
+            todos.stream().filter(todo -> Objects.equals(todo.getId(), id)).findFirst().ifPresentOrElse(todos::remove, () -> {
+                throw new ToDoNotFound("User with id " + id + " not found.");
+            });
         } else {
             throw new ToDoNotFound("There is no todo matching this id: ");
         }
@@ -68,15 +68,73 @@ public class ToDoService {
     }
 
     public void bindTodoToUser(String userEmail, UUID todoUUID) {
-        commonRepository.getTodos().stream()
-                .filter(todo -> todo.getId().equals(todoUUID))
-                .findFirst()
-                .ifPresentOrElse(todo -> todo.setOwnerEmail(userEmail),
-                        () -> {
-                            throw new ToDoNotFound("Todo with ID " + todoUUID + " not found.");
-                        });
+        commonRepository.getTodos().stream().filter(todo -> todo.getId().equals(todoUUID)).findFirst().ifPresentOrElse(todo -> todo.setOwnerEmail(userEmail), () -> {
+            throw new ToDoNotFound("Todo with ID " + todoUUID + " not found.");
+        });
     }
 
+
+    public void printAllCompleted() {
+        commonRepository.getTodos().stream().filter(Todo::isCompleted).forEach(System.out::println);
+    }
+
+    public void printAllUnCompleted() {
+        commonRepository.getTodos().stream().filter(todo -> !todo.isCompleted()).forEach(System.out::println);
+    }
+
+    public void printAllCompletedTodosForSpecificUser(String email) {
+        Optional<User> optionalUser = commonRepository.getUsers().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            commonRepository.getTodos()
+                    .stream()
+                    .filter(todo -> todo.getOwnerEmail().equalsIgnoreCase(user.getEmail()) && todo.isCompleted())
+                    .forEach(System.out::println);
+        } else {
+            throw new UserNotFound("User with email : " + email + " not found!");
+        }
+
+    }
+
+    public void printAllUnCompletedTodosForSpecificUser(String email) {
+        Optional<User> optionalUser = commonRepository.getUsers().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            commonRepository.getTodos()
+                    .stream()
+                    .filter(todo -> todo.getOwnerEmail().equalsIgnoreCase(user.getEmail()) && !todo.isCompleted())
+                    .forEach(System.out::println);
+        } else {
+            throw new UserNotFound("User with email : " + email + " not found!");
+        }
+
+    }
+
+    public void kpiComputingForIndividualUser(String email) {
+        Optional<User> optionalUser = commonRepository.getUsers().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
+
+        if (optionalUser.isPresent()) {
+            List<Todo> userTodos = commonRepository.getTodos().stream()
+                    .filter(todo -> todo.getOwnerEmail().equalsIgnoreCase(email))
+                    .toList();
+
+            long completedCount = userTodos.stream().filter(Todo::isCompleted).count();
+            long uncompletedCount = userTodos.size() - completedCount;
+
+            if (completedCount > uncompletedCount && (completedCount - uncompletedCount) < 5) {
+                System.out.println("Status: Suitable for KPI");
+            } else {
+                System.out.println("Status: Not Suitable for KPI");
+            }
+
+            System.out.println("User: " + optionalUser.get());
+            System.out.println("Total : " + optionalUser.get());
+            System.out.println("Completed Todos: " + completedCount);
+            System.out.println("Uncompleted Todos: " + uncompletedCount);
+        }
+    }
 
     public void printAllTodos() {
         commonRepository.getTodos().forEach(System.out::println);
